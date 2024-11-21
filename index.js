749bb6544944fc92cd7e72abb8c0cd2e7dd6f4f2
Ash <154726037+aarij0119@users.noreply.github.com>
2024-11-21 22:59:21 +0530

    worked on cirlce feature

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
 const canvas = document.querySelector('#whiteboard');
 const context = canvas.getContext('2d')
 const buttons = document.querySelectorAll('#btn1');
-const circle = document.querySelector('#circle')
+const circle = document.querySelector('#circle');
 
 
+let drawMode = "line";
+let circle2 = 'circle'
 let isDraw = false;
 let linewidth = 2
 
-window.addEventListener('load',function(){
+window.addEventListener('load', function () {
     canvas.width = canvas.offsetWidth
     canvas.height = canvas.offsetHeight
 })
 
 // context.beginPath();
 
-buttons.forEach(function(button){
-    button.addEventListener("click",function(e){
-     linewidth = e.target.value
+buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+        linewidth = e.target.value
     })
+})
+
+const drwawCircle = (e) => {
+    canvas.addEventListener('mousedown', drwawCircle)
+    let radius = 50;
+    context.beginPath()
+    context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI * 2)
+}
+
+circle.addEventListener("click", function () {
+    drwawCircle()
 })
 
 const StartDraw = () => {
     isDraw = true
-    context.beginPath();
-    context.lineWidth = linewidth
+    if (drawMode === 'line') {
+        context.beginPath();
+        context.lineWidth = linewidth
+    }
 }
 
-const Draw = (e) =>{
-    if(!isDraw) return
+
+const Draw = (e) => {
+    if (!isDraw) return
     context.lineTo(e.offsetX, e.offsetY);
     context.stroke()
 }
 
 canvas.addEventListener('mousemove', Draw)
 canvas.addEventListener('mousedown', StartDraw)
-canvas.addEventListener('mouseup', ()=> isDraw = false)
+canvas.addEventListener('contextmenu',drwawCircle )
+
+canvas.addEventListener('mouseup', () => isDraw = false)
